7edf2555b0c5e1ac17970af7446c9898bea97fc3
MiniOcean404 <1037306928@qq.com>
2023-11-01 22:51:47 +0800

    feat: template

---
+++ .eslintrc.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "@electron-toolkit", "@electron-toolkit/eslint-config-prettier"],
+}
---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,7 @@
+// import icons from './assets/icons.svg'
+
+function App() {
+  return <div className="container">22222222222222223</div>
+}
+
+export default App
